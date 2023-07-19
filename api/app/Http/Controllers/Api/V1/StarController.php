17ee4fb221523026703024ec78cde69c2d36b30b
dany.bitard <dany.bitard@gmail.com>
2023-07-20 01:30:40 +0200

    feat(api): Adding api ressources with form requests and resources for Star model

---
+++ api/app/Http/Controllers/Api/V1/StarController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreStarRequest;
+use App\Http\Requests\UpdateStarRequest;
+use App\Http\Resources\StarResource;
+use App\Http\Resources\V1\StarCollection;
+use App\Models\Star;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return new StarCollection(Star::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreStarRequest $request)
+    {
+        return new StarResource(Star::create($request->validated()));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Star $star): JsonResource
+    {
+        return new StarResource($star);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateStarRequest $request, Star $star)
+    {
+        return new StarResource($star->update($request->validated()));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Star $star)
+    {
+        $star->delete();
+        return response()->json("$star->first_name $star->last_name deleted successfully");
+    }
+}
---
+++ api/app/Http/Requests/UpdateStarRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateStarRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => ['required', 'min:3', 'max:30'],
+            'last_name' => ['required', 'min:3', 'max:30'],
+            'description' => ['required'],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'first_name.required' => 'First name is required',
+            'last_name.required' => 'Last name is required',
+            'description.required' => 'Description name is required'
+        ];
+    }
+}
