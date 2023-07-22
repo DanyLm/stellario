2e4d26d85876d89206fc31d6b5282ce7c39dbbd3
dany.bitard <dany.bitard@gmail.com>
2023-07-23 00:04:45 +0200

    feat(backoffice): Include tailwind to module and main

--- backoffice/tailwind.config.js
+++ backoffice/tailwind.config.js
@@ -1,10 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        stellario: '#f0f0f7',
+        'stellario-primary': '#00668A',
+        'stellario-secondary': '#004E71'
+      }
+    },
+    fontFamily: {
+      Roboto: ['Roboto, sans-serif']
+    },
+    container: {
+      padding: '2rem',
+      center: true
+    },
+    screens: {
+      sm: '640px',
+      md: '768px'
+    }
   },
-  plugins: [],
+  plugins: []
 }
-
