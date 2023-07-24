c9d2c66fd7d1315ca7cdb3f99c997a43774b5e6c
dany.bitard <dany.bitard@gmail.com>
2023-07-24 19:57:17 +0200

    feat(app): Adding head and meta

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,4 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'Stellario',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Stellario meta desc'
+        },
+        { name: 'format-detection', content: 'telepgon-no' }
+      ],
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap'
+        },
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.icon'
+        }
+      ]
+    }
+  }
 })
