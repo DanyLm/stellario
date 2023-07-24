68e17b3bffdf80f2a49270c315a957a9515e8ea0
dany.bitard <dany.bitard@gmail.com>
2023-07-24 23:59:35 +0200

    config(app): Adding tailwind configuration with css file

---
+++ app/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['app.vue', './pages/**/*.{html,js,vue,ts}', './components/**/*.{html,js,vue,ts}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
