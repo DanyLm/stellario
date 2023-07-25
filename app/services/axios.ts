6ad89b9ac6e0c82cc35d6e0c8ac050649ad79598
dany.bitard <dany.bitard@gmail.com>
2023-07-25 10:39:56 +0200

    fix(app): Fix issue about env variable on axios

--- app/services/axios.ts
+++ app/services/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
-const config: any = useRuntimeConfig()
-const BASE_URL: string = config.public.apiBase
+// const config: any = useRuntimeConfig()
+// const BASE_URL: string = config.public.apiBase
+const BASE_URL: string = "http://localhost:8000/api/v1"
 
 export default axios.create({
   baseURL: BASE_URL
